c1c0b0c8d84616d247e37ba54d45a66aa3c1762c
zainab7681051 <zainab76810510@gmail.com>
2025-02-23 22:06:13 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 
-// function animate_icons() {
-// 	const tl=gsap.timeline();
-// 	tl.to
-// }
-function animate(){
+gsap.registerPlugin(ScrollTrigger);
+
+function gsap_animate(){
 	const tl = gsap.timeline();
-	tl.to(".navigation", {opacity:1, y:0, duration:1, ease: "power2.inOut"},0)
-	.to(".section-title-primary", {opacity:1, y:0, duration:1, ease: "power2.inOut"},0)
-	.to(".section-title-secondary", {opacity:1, y:0, duration:1, ease: "power2.inOut"},0)
-	.to(".hero-img-container", {opacity:1, y:0, duration:1, ease: "power2.inOut"},0)
-	.to(".popup-button",{opacity:1, duration:2,ease: "power2.inOut"}, 0.1)
+	return tl.to(".navigation", {opacity:1, y:0, duration:1, ease: "power2.in"},0)
+	.to(".section-title-primary", {opacity:1, y:0, duration:1, ease: "power2.in"},0)
+	.to(".section-title-secondary", {opacity:1, y:0, duration:1, ease: "power2.in"},0)
+	.to(".hero-img-container", {opacity:1, y:0, duration:1, ease: "power2.in"},0)
 	.from(
 	  ".banana-icon, .espresso-icon, .grapes-icon, .mango-icon, .rasperry-icon, .strawberry-icon",
 	  { x: "-50vh",
@@ -20,7 +18,39 @@ function animate(){
 	    ease: "power3.out",
 	    stagger: 0.2
 	  }, 0.1)
+	.from(".popup-button",{opacity:0, duration:2,ease: "power2.out"}, 0.1)
+	.to(".popup-button", {
+	  scale: 1,
+	  ease: "power2.inOut",
+	  duration:0.5,
+	  scrollTrigger: {
+	    trigger: "#intro",
+	    start: "top top",
+	    end: "+=150",
+	    scrub: true,
+	  }
+	}, 0)
+	.to(".popup-button", {
+	  display: "none",
+	  ease: "power2.inOut",
+	  duration:0.5,
+	  scrollTrigger: {
+	    trigger: "footer",
+	    start: "top bottom",
+	    end: "top center",
+	    scrub: true
+	  }
+	}, 0)
+	.to(".text-on-bubble-img", {opacity:1, y:0, duration:3, ease: "power2.in",
+		scrollTrigger:{
+			trigger:".text-on-bubble-img",
+			start: "top center",
+			end:"top top",
+			scrub:false
+		}
+	},1)
 }
 addEventListener("load", () =>{
-	animate();
+    const mm = gsap.matchMedia();
+    mm.add('(min-width: 1024px)', ()=>gsap_animate())
 })
