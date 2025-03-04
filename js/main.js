aaeb896454b41b7f3d85940a782885b672baad07
zainab7681051 <zainab76810510@gmail.com>
2025-03-05 01:55:00 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -12,15 +12,15 @@ class HeroSlide{
 		this.current_index = this.current_index % this.total_slides;
 		this.toggle_elements_class(this.slides, this.current_index, "hero-slide-active");
 		this.toggle_elements_class(this.slide_icon_btns, this.current_index, "icon-btn-active")
-		this.change_hero_bg_color();
+		this.change_hero_bg_color(this.slides[this.current_index]);
 		this.current_index++;
 	}
 	toggle_elements_class(element, index, className, ){
 		element.forEach(e => e.classList.remove(className));
 		element[index].classList.add(className);
 	}
-	change_hero_bg_color(){
-		const bg_color = document.querySelector(".hero-bg");
+	change_hero_bg_color(slide){
+		const bg_color = slide.querySelector(".hero-bg");
 		const hero_txt_img_container = document.querySelector(".hero-txt-img-container");
 		let color = getComputedStyle(bg_color).backgroundColor;
 		console.log("color: ", color);
